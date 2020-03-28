2356631c98d671533f79a8877c501c322e2e0d1a
Bhanu pratap singh <pbhanu314@gmail.com>
2020-03-28 12:53:48 +0530

    problem 5 Added

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,17 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+import problem1.*;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+    public void insert(int data){
+        TreeNode node = new TreeNode(data)
+        if(root==null)
+
+    }
 }
--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -6,5 +6,32 @@
  */
 package problem5.node;
 // to define node properties
+
+import problem5.student.Student;
+
 public class Node {
+    private Student s;
+    private Node next;
+    public Node(Student s) {
+        this.s = s;
+        next = null;
+    }
+
+
+    public Student getS() {
+        return s;
+    }
+
+    public void setS(Student s) {
+        this.s = s;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
